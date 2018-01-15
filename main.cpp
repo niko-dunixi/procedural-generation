5e3caedb7d35b069b16916ddf5377e2dec5f889f
Paul Baker <paul.nelson.baker@gmail.com>
2018-01-15 00:16:26 -0700

    Main method generates simple random numbers.
    TODO: Extrapolate into a simple reusable class.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <random>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    // creating random numbers for c++ 11
+    // https://stackoverflow.com/a/19666713/1478636
+    std::random_device rd;
+    std::mt19937 mt(rd());
+//    std::seed_seq seed{rd()};
+//    std::mt19937 mt(seed);
+    std::uniform_real_distribution<double> dist(1.0, 10.0);
+    for (int i = 0; i < 10; i++){
+        std::cout << "Here, have a rando: " << dist(mt) << " " << std::endl;
+    }
     return 0;
 }
